8fbda939b1608ccc707338c7e12f09dc2c3fff63
Allen Mathews <allenjoemathews@gmail.com>
2023-08-18 20:17:55 +1000

    dione

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-ReactDOM.render( <h1 > Hello Everyone!My name is Allen and I am a front-end developer based in Melbourne, Australia </h1>, document.getElementById("root"))
+ReactDOM.render(<h1>Hello everyone, my name is Allen </h1>, document.getElementById("root"))
